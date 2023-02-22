7740b4246804553c748db613b48a4b0d18ae9e20
isha-web1 <isha.web1@gmail.com>
2023-02-22 23:22:30 +0600

    problem-two added

---
+++ problem-two.js
@@ -0,0 +1,31 @@
+// 1  .  You have an array [ 1, 9, 17, 22 ]. Add the all elements
+//       of this array and give output. Do this using for loop &
+//       array.reduce() method.
+
+
+// using for loop
+
+// const numbers = [1, 9, 17, 22];
+// const total = [];
+// for(number of numbers){
+//     const sumNum = number + 1;
+//     total.push(sumNum);
+
+// }
+// console.log(total);
+
+
+// using reduce() methode
+
+
+// const numbers = [1, 9, 17, 22];
+
+// const total = numbers.reduce((previous, current) => 
+//    {return previous + current}, 0);
+
+//    console.log(total);
+
+//    ans : 49
+
+
+
